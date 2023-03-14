1a51639452e1a5c0b87ea83a224aefe9abf586d0
yohandiaz <56124419+yohandiaz@users.noreply.github.com>
2023-03-14 21:01:25 +0100

    Update audioToText.py

--- audioToText.py
+++ audioToText.py
@@ -4,19 +4,24 @@ import pathlib
 import speech_recognition as sr
 engine = sr.Recognizer()
 
-# read mp3 file
-
 audio_file_path = str(pathlib.Path(__file__).parent.resolve()) + "/"
 audio_file_path += input("Insert the name of the file: ")
-convertedFileName = fl.load_audio_file(audio_file_path)
 
-with sr.AudioFile(convertedFileName) as source:
-    print('Analyzing file...')
-    audio = engine.record(source)
+audioToText(audio_file_path)
 
-# extract and print text
+# read mp3 file
+def audioToText(file_path):
+    
+    # Converts the audio file to wav format if it is not
+    convertedFileName = fl.load_audio_file(file_path)
 
-text = engine.recognize_google(audio, language="es-ES")
-print(f'Text: {text}')
-txtFile = open('textRecognized.txt', 'a')
-txtFile.writelines(text)
+    with sr.AudioFile(convertedFileName) as source:
+        print('Analyzing file...')
+        audio = engine.record(source)
+
+    # extract and print text
+
+    text = engine.recognize_google(audio, language="es-ES")
+    print(f'Text: {text}')
+    txtFile = open('textRecognized.txt', 'a')
+    txtFile.writelines(text)
