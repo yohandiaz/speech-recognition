0bbfdc65aade8181bb14f7df2cdae05ce7ffce41
yohandiaz <56124419+yohandiaz@users.noreply.github.com>
2023-03-14 03:30:04 +0100

    Update audioToText.py

--- audioToText.py
+++ audioToText.py
@@ -1,11 +1,12 @@
 import file_loader as fl
+import pathlib
 
 import speech_recognition as sr
 engine = sr.Recognizer()
 
 # read mp3 file
 
-audio_file_path = "/home/yohan/speech-recognition/"
+audio_file_path = pathlib.Path(__file__).parent.resolve()
 audio_file_path += input("Insert the name of the file: ")
 convertedFileName = fl.load_audio_file(audio_file_path)
 
